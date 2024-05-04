5c1409f28d98c0ae210c198929cb57ae4273c1e8
Jay <jayakk@hotmail.com>
2024-05-04 18:04:49 -0400

    Initial commit with Spriong Boot initializr prok

---
+++ src/main/java/com/kuttikkattu/www/KLGLottoFramework/KlgLottoFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.kuttikkattu.www.KLGLottoFramework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlgLottoFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlgLottoFrameworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kuttikkattu/www/KLGLottoFramework/KlgLottoFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuttikkattu.www.KLGLottoFramework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlgLottoFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
